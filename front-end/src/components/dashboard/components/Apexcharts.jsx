a8bd665fe0e02de439fe59437abb4613fe0ee702
Jr <jubayerrahman203@gmail.com>
2023-07-25 07:44:42 +0600

    gfr

---
+++ front-end/src/components/dashboard/components/Apexcharts.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+const ChartComponent = () => {
+  // Sample data for the chart
+  const chartData = {
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
+    },
+    series: [
+      {
+        name: "Sales",
+        data: [30, 40, 45, 50, 49, 60, 70, 91, 125, 100, 95, 80],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <ReactApexChart
+        options={chartData.options}
+        series={chartData.series}
+        type="bar"
+        height={350}
+      />
+    </div>
+  );
+};
+
+export default ChartComponent;
--- front-end/src/components/dashboard/pages/Home/HomeD.jsx
+++ front-end/src/components/dashboard/pages/Home/HomeD.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { FaPeopleGroup, FaTags } from "react-icons/fa6";
+import { GrArticle } from "react-icons/gr";
+import { MdCategory } from "react-icons/md";
+import HomeTopSingel from "../../components/HomeTopSingel";
+import ChartComponent from "../../components/Apexcharts";
 
 const HomeD = () => {
-  return <div>HomeD</div>;
+  return (
+    <div className="HomeD">
+      <div className="home__top">
+        <HomeTopSingel count={45} icon={<FaPeopleGroup />} name={"visitors"} />
+        <HomeTopSingel count={145} icon={<GrArticle />} name={"articles"} />
+        <HomeTopSingel count={34} icon={<MdCategory />} name={"categorys"} />
+        <HomeTopSingel count={94} icon={<FaTags />} name={"tags"} />
+      </div>
+
+      <ChartComponent />
+    </div>
+  );
 };
 
 export default HomeD;
